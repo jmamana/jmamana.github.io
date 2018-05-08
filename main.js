156da698368c0d4b08c22fc087c37cd8b67a421a
jmamana <jay.mamana@gmail.com>
2018-05-08 10:41:41 -0400

    Update main.js

--- main.js
+++ main.js
@@ -6,3 +6,21 @@ document.getElementById('mute').addEventListener('click', function (e)
     audio.muted = !audio.muted;
     e.preventDefault();
 }, false);
+
+// When the user scrolls the page, execute myFunction 
+window.onscroll = function() {myFunction()};
+
+// Get the navbar
+var topnav = document.getElementById("topnav");
+
+// Get the offset position of the navbar
+var sticky = topnav.offsetTop;
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    topnav.classList.add("sticky")
+  } else {
+    topnav.classList.remove("sticky");
+  }
+}
